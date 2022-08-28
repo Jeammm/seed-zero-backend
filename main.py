76d49160dee78077d8bbc79ef6e4f5295068f5e0
Athicha <athicha.phjkl@gmail.com>
2022-08-28 11:14:43 +0700

    yay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from http.client import HTTPException
 from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
+from pydantic import BaseModel
 import os
 load_dotenv()
 
@@ -11,29 +12,37 @@ NAME = os.getenv('NAME', default="World")
 app = FastAPI()
 
 # TODO: 1.) Add endpoint as a top-level page.
-@app.get("")
+@app.get("/")
 def index():
     return {"message": f"Hello {NAME}"}
 
 # TODO: 2.) Create a calculator that accept 2 query params and manipulate those 2 values.
 @app.get("/calculator/add")
 def adder(first_number: int, second_number: int):
-    # TODO: Start here
-    return {"first_number": first_number, "second_number": second_number, "result": 0}
+    result = first_number + second_number
+    return {"first_number": first_number, "second_number": second_number, "result": result}
 
 @app.get("/calculator/substract")
 def subtractor(first_number: int, second_number: int):
-    # TODO: Start here
-    return {"first_number": first_number, "second_number": second_number, "result": 0}
+    result = first_number - second_number
+    return {"first_number": first_number, "second_number": second_number, "result": result}
 
 # TODO: (Optional) @app.get("/calculator/multiply")
+@app.get("/calculator/multiply")
+def subtractor(first_number: int, second_number: int):
+    result = first_number * second_number
+    return {"first_number": first_number, "second_number": second_number, "result": result}
+
 
 # TODO: (Optional) @app.get("/calculator/divide")
-
+@app.get("/calculator/divide")
+def subtractor(first_number: int, second_number: int):
+    result = first_number / second_number
+    return {"first_number": first_number, "second_number": second_number, "result": result}
 
 
 # TODO: 3.) Enter a path scheme that accept userId as a path parameter `userId`.
-@app.get("")
+@app.get("/userid/{userId}")
 def usersInfo(userId: int):
     if userId != 9997:
         raise HTTPException(status_code=404, detail="UserId not found.")
@@ -51,9 +60,12 @@ def usersInfo(userId: int):
 #   "name": "Book",
 #   "price": 123,
 # }
-# @app.????(???)
-def itemCreator(item: Item):
+class Item(BaseModel):
+    name : str
+    price : int
+
+@app.post("/item/create/")
+async def itemCreator(item: Item):
     # TODO: Print body's name and price to console.
+    print(item.name, item.price)
     return item.name, item.price
-
-
